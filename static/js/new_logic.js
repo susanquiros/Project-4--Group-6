5d8bf625bfd998d03326a1f7f89e780620bca729
adamholcomb <adam.holcomb@ymail.com>
2021-10-06 20:55:57 -0500

    Add slides

--- static/js/new_logic.js
+++ static/js/new_logic.js
@@ -7,9 +7,29 @@ d3.select("#clickMe").on("click", function (e) {
   d3.json(`http://127.0.0.1:5000/api/winedata/${sel1},${sel2},${sel3}`).then(function (data) {
     d3.select(".result").text(data['Recommended 1']);
     d3.select(".result2").text(data['Recommended 2']);
+    console.log(data)
     
     // d3.select()
   });
 
   
+});
+
+btnParty.addEventListener("click", () => {
+  confetti("tsparticles", {
+    angle: 90,
+    count: 25,
+    position: { x: 50, y: 50 },
+    spread: 90,
+    startVelocity: 60,
+    decay: 0.9,
+    gravity: 1,
+    drift: 0,
+    ticks: 200,
+    colors: ["#fff", "#f00"],
+    shapes: ["square", "circle"],
+    scalar: 1,
+    zIndex: 2000,
+    disableForReducedMotion: true
+  });
 });
